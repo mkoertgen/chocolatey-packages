678c2522d45cbe743bd9f0f931e9531bcb7082ef
Marcel Körtgen <marcel.koertgen@gmail.com>
2022-08-27 13:47:35 +0200

    Finish au package

--- automatic/visualvm/tools/chocolateyinstall.ps1
+++ automatic/visualvm/tools/chocolateyinstall.ps1
@@ -1,37 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = '{{DownloadUrl}}' # download url, HTTPS preferred
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  url           = $url
-  softwareName  = 'visualvm*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+$packageName = $env:ChocolateyPackageName
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-  # Checksums are now required as of 0.10.0.
-  # To determine checksums, you can get that from the original site if provided.
-  # You can also use checksum.exe (choco install checksum) and use it
-  # e.g. checksum -t sha256 -f path\to\file
-  checksum      = '{{Checksum}}'
-  checksumType  = '{{ChecksumType}}' #default is md5, can also be sha1, sha256 or sha512
-}
+$url = 'https://github.com/oracle/visualvm/releases/download/2.1.4/visualvm_214.zip'
+$checksum = 'bf6c116fd868f8c6f15af3f6e9b5454f21cfffd7a5ecfbb6c653e278ac400f7d'
 
-Install-ChocolateyZipPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateyzippackage
-## Download and unpack a zip file - https://docs.chocolatey.org/en-us/create/functions/install-chocolateyzippackage
-##Install-ChocolateyZipPackage $packageName $url $toolsDir [$url64 -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64]
-
-## Unzips a file to the specified location - auto overwrites existing content
-## - https://docs.chocolatey.org/en-us/create/functions/get-chocolateyunzipp
-#Get-ChocolateyUnzip "FULL_LOCATION_TO_ZIP.zip" $toolsDir
-
-## Add specific files as shortcuts to the desktop
-## - https://docs.chocolatey.org/en-us/create/functions/install-chocolateyshortcut
-#$target = Join-Path $toolsDir "$($packageName).exe"
-# Install-ChocolateyShortcut -shortcutFilePath "<path>" -targetPath "<path>" [-workDirectory "C:\" -arguments "C:\test.txt" -iconLocation "C:\test.ico" -description "This is the description"]
-
-
-##PORTABLE EXAMPLE
-#$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-# despite the name "Install-ChocolateyZipPackage" this also works with 7z archives
-#Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
-## END PORTABLE EXAMPLE
+Install-ChocolateyZipPackage $packageName $url $toolsDir -checksum $checksum -checksumType 'sha256'
--- automatic/visualvm/update.ps1
+++ automatic/visualvm/update.ps1
@@ -3,19 +3,27 @@ param([switch] $Force)
 
 Import-Module AU
 
-$domain   = 'https://github.com'
+$domain = 'https://github.com'
 $releases = "$domain/oracle/visualvm/releases/latest"
 
 function global:au_BeforeUpdate {
-  Get-RemoteFiles -Purge -NoSuffix -FileNameBase "visualvm"
+  #Get-RemoteFiles -Purge -NoSuffix -FileNameBase "visualvm"
 }
 
 function global:au_SearchReplace {
   @{
-    ".\legal\VERIFICATION.txt" = @{
+    "$($Latest.PackageName).nuspec" = @{
+      "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+    }
+
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+      "(?i)(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+    }
+
+    ".\legal\VERIFICATION.txt"      = @{
       "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$($Latest.ReleaseURL)>"
       "(?i)(^\s*software.*)\<.*\>"        = "`${1}<$($Latest.URL64)>"
-      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
       "(?i)(^\s*checksum\:).*"            = "`${1} $($Latest.Checksum64)"
     }
   }
@@ -25,17 +33,14 @@ function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
   $re = '\.zip$'
-  $url = $download_page.links | ? href -match $re | % href | select -First 1
-
-  #Write-Host $url
-
+  $url = $download_page.links | Where-Object href -match $re | ForEach-Object href | Select-Object -First 1
   $version = (Split-Path ( Split-Path $url ) -Leaf)
 
   return @{
-    Version     = $version
-    URL64       = "$domain/$url"
-    ReleaseURL  = "$domain/oracle/visualvm/releases/tag/${version}"
+    Version    = $version
+    URL64      = "$domain$url"
+    ReleaseURL = "$domain/oracle/visualvm/releases/tag/${version}"
   }
 }
 
-update -ChecksumFor none -Force:$Force
+update
