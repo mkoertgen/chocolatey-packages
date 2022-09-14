b55b1b885975d4a53e4e5a6d0b8d4cc2b8dc272d
Marcel Körtgen <marcel.koertgen@gmail.com>
2022-09-14 10:10:58 +0200

    (visualvm) Use splatting in install script

--- automatic/visualvm/tools/chocolateyinstall.ps1
+++ automatic/visualvm/tools/chocolateyinstall.ps1
@@ -1,9 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
-$packageName = $env:ChocolateyPackageName
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  url = 'https://github.com/oracle/visualvm/releases/download/2.1.4/visualvm_214.zip'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  checksum = 'bf6c116fd868f8c6f15af3f6e9b5454f21cfffd7a5ecfbb6c653e278ac400f7d'
+  checksumType = 'sha256'
+}
 
-$url = 'https://github.com/oracle/visualvm/releases/download/2.1.4/visualvm_214.zip'
-$checksum = 'bf6c116fd868f8c6f15af3f6e9b5454f21cfffd7a5ecfbb6c653e278ac400f7d'
-
-Install-ChocolateyZipPackage $packageName $url $toolsDir -checksum $checksum -checksumType 'sha256'
+Install-ChocolateyZipPackage @packageArgs
